1a4ddf9300e713bd3451bcb185e34fde8ab39fe2
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-10-04 00:50:27 +0300

    Add beauty

--- TracerLibrary/TracerLibrary/ITracer.cs
+++ TracerLibrary/TracerLibrary/ITracer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+﻿
 namespace TracerLibrary
 {
     public interface ITracer
--- TracerLibrary/TracerLibrary/TraceResult.cs
+++ TracerLibrary/TracerLibrary/TraceResult.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using System.Collections.Immutable;
-using System.Diagnostics;
 
 namespace TracerLibrary
 {
